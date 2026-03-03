c0686f6ae2c51d461c0401ab5fdebbce7bf483c6
WY <hugkangaroo@gmail.com>
2026-03-03 17:07:15 +0800

    Fix bug

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ if question:
 
         for col in num_cols:
             if col in df.columns:
-            df[col] = df[col].map(lambda x: f"{x:,.0f}") 
+                df[col] = df[col].map(lambda x: f"{x:,.0f}") 
         
         st.dataframe(df)
 
